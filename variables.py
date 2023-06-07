13835bb1d539e95d2427814821152ac85d3a8453
Hamid-Git7 <23.imraan@gmail.com>
2023-06-07 16:56:47 +0200

    Premier commit

---
+++ variables.py
@@ -0,0 +1,46 @@
+number7 = 145
+number7 = 40
+print(number7)
+
+number3 = 5.77
+number3 = 2.36
+print(number3)
+
+text3 = 'Hello Daishi'
+print(text3)
+
+text2 = "Goodbye Daishi"
+print(text2)
+
+text7 = """<div>
+    <h1>Titre de premier niveau</h1>
+    </div>
+"""
+print(text7)
+
+football_is_good = True
+print(football_is_good)
+
+football_is_bad = False
+print(football_is_bad)
+foo = "None"
+user_accepted_terms = None
+print(user_accepted_terms)
+
+print(type(user_accepted_terms))
+print(type(foo))
+
+print(type(number7))
+print(type(number3))
+print(type(text3))
+print(type(text2))
+print(type(text7))
+print(type(football_is_good))
+print(type(football_is_bad))
+
+print(type(number3) is int)
+print(type(number3) is str)
+
+print(str(number7))
+print(int(number7))
+print(type(str(number7)))
