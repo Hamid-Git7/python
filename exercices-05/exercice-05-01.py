5b575bff54bc026701164613b3fc197c45cac550
Hamid-Git7 <23.imraan@gmail.com>
2023-06-19 16:04:51 +0200

    Exercice05

---
+++ exercices-05/exercice-05-01.py
@@ -0,0 +1,14 @@
+# exo 5.1
+# Ajoutez de la documentation à la fonction suivante et vérifiez
+# qu'elle s'affiche correctement en utilisant la fonction help()
+
+# réponse 5.1
+
+def multiplication(a: float, b: float) -> float:
+    return a * b
+
+# la fonction multiplication permet de multiplier la float "a" et la float "b", (float est utilisé pour stocker des nombres à virgules flottante)
+#nous aurons donc un résultat a virgules.
+
+print(multiplication( 2.5, 3.14))
+
